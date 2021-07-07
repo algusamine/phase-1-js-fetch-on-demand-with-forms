a333b270313e55296a87d79af99180fead4b53bb
Amine <algusamine@gmail.com>
2021-07-06 23:55:13 -0400

    commit tree 2f2e8b4064abdee10bf8b0cc583f648f93370eba

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (event)=>{
+      event.preventDefault();
+      console.log(event);
+     // event.target.children[1].value  (didn't work)
+      const input = document.querySelector('#searchByID');
+      console.log(input.value);
+
+      fetch ('http://localhost:3000/movies')
+      .then(resp => resp.json())
+      .then(data => {
+          const title = document.getElementsByName('h4');
+          const summary = document.getElementsByTagName('p');
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+      });
+  });
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
